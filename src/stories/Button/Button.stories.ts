48a17e66481cbd02438dc34276031ba49ee8c8dd
ferdinandlaine <laineferdinand@gmail.com>
2025-08-06 18:32:24 +0200

    Button improvement in progress

--- src/stories/Button/Button.stories.ts
+++ src/stories/Button/Button.stories.ts
@@ -1,66 +1,70 @@
 import type { Meta, StoryObj } from '@storybook/vue3-vite';
 import { IconHeart } from '@tabler/icons-vue';
-import { fn } from 'storybook/test';
 import Button from './Button.vue';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
   title: 'Components/Button',
   component: Button,
   tags: ['autodocs'],
   argTypes: {
-    size: { control: 'select', options: ['base', 'small'] },
-    variant: { control: 'select', options: ['primary', 'red', 'green'] },
-    expanded: { control: 'boolean' },
+    size: { control: 'select', options: ['base', 'sm'] },
+    variant: { control: 'select', options: ['primary', 'success', 'danger'] },
     icon: { control: false },
     iconOnly: { control: 'boolean' },
-    disabled: { control: 'boolean' }
+    disabled: { control: 'boolean' },
+    default: { table: { disable: true } } // remove slot from Storybook interface
   },
   args: {
     label: 'Button',
-    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-    onClick: fn()
+    size: 'base'
   }
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: { variant: undefined }
-};
-
 export const Primary: Story = {
   args: { variant: 'primary' }
 };
 
-export const Green: Story = {
-  args: { variant: 'green' }
+export const Success: Story = {
+  args: { variant: 'success' }
 };
 
-export const Red: Story = {
-  args: { variant: 'red' }
+export const Danger: Story = {
+  args: { variant: 'danger' }
+};
+
+export const Default: Story = {
+  args: { variant: undefined }
 };
 
 export const Small: Story = {
-  args: { size: 'small' }
-};
-
-export const Expanded: Story = {
-  args: { expanded: true }
-};
-
-export const Disabled: Story = {
-  args: { disabled: true }
+  args: {
+    variant: 'primary',
+    icon: IconHeart,
+    size: 'sm'
+  }
 };
 
 export const WithIcon: Story = {
-  args: { icon: IconHeart }
+  args: {
+    variant: 'primary',
+    icon: IconHeart
+  }
 };
 
 export const IconOnly: Story = {
   args: {
+    variant: 'primary',
     icon: IconHeart,
     iconOnly: true
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    variant: 'primary',
+    disabled: true
   }
 };
--- src/stories/Button/Button.vue
+++ src/stories/Button/Button.vue
@@ -1,20 +1,32 @@
 <template>
-  <button type="button" :class="classes" :disabled @click="onClick">
-    <component :is="icon" v-if="icon" aria-hidden="true" size="1.25em" />
-    <span :class="{ 'sr-only': iconOnly }">{{ label }}</span>
+  <button
+    :class="[
+      'ui-control flex cursor-pointer items-center justify-center border-transparent! font-medium',
+      buttonVariant,
+      buttonSize
+    ]"
+    :disabled
+  >
+    <slot />
+
+    <template v-if="!(slots.default && slots.default().length)">
+      <component v-if="icon" :is="icon" aria-hidden="true" size="1.25em" />
+      <span :class="{ 'sr-only': iconOnly }">{{ label }}</span>
+    </template>
   </button>
 </template>
 
 <script lang="ts" setup>
+import { computed, useSlots } from 'vue';
 import type { Icon } from '@tabler/icons-vue';
-import { computed } from 'vue';
+
+const slots = useSlots();
 
 const props = withDefaults(
   defineProps<{
-    label: string;
-    size?: 'base' | 'small';
-    variant?: 'primary' | 'red' | 'green';
-    expanded?: boolean;
+    label?: string;
+    size?: 'base' | 'sm';
+    variant?: 'primary' | 'success' | 'danger';
     icon?: Icon;
     iconOnly?: boolean;
     disabled?: boolean;
@@ -24,34 +36,39 @@ const props = withDefaults(
   }
 );
 
-const emit = defineEmits<{
-  (e: 'click', id: number): void;
-}>();
+const variants = {
+  primary:
+    'text-white bg-primary-500 hover:bg-primary-600 focus:bg-primary-600 focus:ring-primary-300 active:bg-primary-700 disabled:bg-primary-300 dark:disabled:bg-primary-800! dark:disabled:text-gray-500!',
+  success:
+    'text-white bg-green-500 hover:bg-green-600 focus:bg-green-600 focus:ring-green-300 active:bg-green-700 disabled:bg-green-300 dark:disabled:bg-green-800 dark:disabled:text-gray-500',
+  danger:
+    'text-white bg-red-500 hover:bg-red-600 focus:bg-red-600 focus:ring-red-300 active:bg-red-700 disabled:bg-red-300 dark:disabled:bg-red-800 dark:disabled:text-gray-500'
+};
 
-const classes = computed(() => [
-  'flex items-center justify-center font-medium cursor-pointer outline-none transition disabled:cursor-not-allowed focus:ring-3',
+const buttonVariant = computed(
+  () =>
+    (props.variant && variants[props.variant]) ??
+    'hover:bg-gray-50 focus:bg-gray-50 focus:ring-gray-300 active:bg-gray-100'
+  // not-disabled:dark:hover:bg-gray-900 not-disabled:dark:focus:bg-gray-900 not-disabled:dark:active:bg-gray-800 disabled:bg-transparent disabled:text-gray-500
+);
 
-  // SIZES
+const buttonSize = computed(() => [
   props.size === 'base' && 'gap-2 rounded-lg',
-  props.size === 'base' && !props.iconOnly && 'px-3 py-2',
-  props.size === 'base' && props.iconOnly && 'size-10',
-  props.size === 'small' && 'gap-1.5 text-sm rounded-md',
-  props.size === 'small' && !props.iconOnly && 'px-2 py-1.5',
-  props.size === 'small' && props.iconOnly && 'size-8',
-  props.expanded && 'w-full',
-
-  // VARIANTS
-  !props.variant &&
-    'hover:bg-gray-50 focus:bg-gray-50 focus:ring-gray-300 active:bg-gray-100 not-disabled:dark:hover:bg-gray-900 not-disabled:dark:focus:bg-gray-900 not-disabled:dark:active:bg-gray-800 disabled:bg-transparent disabled:text-gray-500',
-  props.variant === 'primary' &&
-    `bg-primary-500 text-white hover:bg-primary-600 focus:bg-primary-600 focus:ring-primary-300 active:bg-primary-700 disabled:bg-primary-100 disabled:text-primary-600 dark:disabled:bg-primary-800 dark:disabled:text-primary-300`,
-  props.variant === 'green' &&
-    `bg-green-500 text-white hover:bg-green-600 focus:bg-green-600 focus:ring-green-300 active:bg-green-700 disabled:bg-green-100 disabled:text-green-600 dark:disabled:bg-green-800 dark:disabled:text-green-300`,
-  props.variant === 'red' &&
-    `bg-red-500 text-white hover:bg-red-600 focus:bg-red-600 focus:ring-red-300 active:bg-red-700 disabled:bg-red-100 disabled:text-red-600 dark:disabled:bg-red-800 dark:disabled:text-red-300`
+  // props.size === 'base' && !props.iconOnly && 'px-3 py-2',
+  props.size === 'base' && props.iconOnly && 'size-10'
+  // props.size === 'sm' && 'gap-1.5 text-sm rounded-md',
+  // props.size === 'sm' && !props.iconOnly && 'px-2 py-1.5',
+  // props.size === 'sm' && props.iconOnly && 'size-2'
 ]);
 
-const onClick = () => {
-  emit('click', 1);
-};
+// const variantClass = computed(() => [
+//   // !props.variant &&
+//   //   'hover:bg-gray-50 focus:bg-gray-50 focus:ring-gray-300 active:bg-gray-100 not-disabled:dark:hover:bg-gray-900 not-disabled:dark:focus:bg-gray-900 not-disabled:dark:active:bg-gray-800 disabled:bg-transparent disabled:text-gray-500',
+//   props.variant === 'primary' &&
+//     `bg-primary-500 text-white hover:bg-primary-600 focus:bg-primary-600 focus:ring-primary-300 active:bg-primary-700 disabled:bg-primary-300 dark:disabled:bg-primary-800 dark:disabled:text-gray-500`,
+//   props.variant === 'green' &&
+//     `bg-green-500 text-white hover:bg-green-600 focus:bg-green-600 focus:ring-green-300 active:bg-green-700 disabled:bg-green-300 dark:disabled:bg-green-800 dark:disabled:text-gray-500`,
+//   props.variant === 'red' &&
+//     `bg-red-500 text-white hover:bg-red-600 focus:bg-red-600 focus:ring-red-300 active:bg-red-700 disabled:bg-red-300 dark:disabled:bg-red-800 dark:disabled:text-gray-500`
+// ]);
 </script>
